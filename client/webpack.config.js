4979faed3c5d69772d4544c0500aa23b8d2f3ca6
Sean Johnson <sjohnson@sjohnsonaz.com>
2020-03-21 21:58:07 -0700

    Added client build.

---
+++ client/webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        'main': './scripts/main.tsx',
+        'styles': './styles/styles.styl',
+        'config': './config/config.ts'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './public/bundle'),
+        libraryTarget: 'var',
+        library: '[name]'
+    },
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+        plugins: [
+            new TsconfigPathsPlugin({
+                configFile: './tsconfig.json'
+            })
+        ]
+    },
+    externals: {
+        mocha: 'mocha',
+        chai: 'chai'
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader'
+        }, {
+            test: /\.styl$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                { loader: 'postcss-loader' },
+                'stylus-loader'
+            ]
+        }, {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'clean-css-loader',
+                { loader: 'postcss-loader' }
+            ]
+        }, {
+            test: /\.(woff|woff2|eot|ttf|svg)$/,
+            use: [
+                'file-loader'
+            ]
+        }]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new OptimizeCssAssetsPlugin({
+            cssProcessorOptions: {
+                discardComments: {
+                    removeAll: true
+                }
+            }
+        })
+    ]
+};
